417bb574b52a29c44227aa6eda0ae41f1a2c3c26
gabriellakodra <gabriella.kodra@gmail.com>
2025-11-17 16:11:27 +0200

    tehty ja korjattu lataa testit

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa_kortille_rahaa_jos_positiivinen_summa(self):
+        maksukortti_mock = Mock()
+        
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called_with(10)
+
+    def test_ei_lataa_kortille_rahaa_jos_negatiivinen_summa(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
+        
+
